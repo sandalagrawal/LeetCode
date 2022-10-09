b0ccee5d8de4f08b47f0b702eec80e68eb8bb087
sandalagrawal01 <86615492+sandalagrawal@users.noreply.github.com>
2022-10-10 03:33:22 +0530

    Time: 37 ms (27.89%), Space: 14.2 MB (56.20%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,41 +9,19 @@
  */
 class Solution {
 public:
-    // this is the first method to find the lowest common ancestor, using path.
-    
-    // using this function we find the paths of the given node from the root node.
-    bool findPath(TreeNode* root, vector<TreeNode*>&path, TreeNode* node)
-    {
-        if(root==NULL)
-            return false;
-        path.push_back(root);
-        if(root==node)
-            return true;
-        if(findPath(root->left,path,node) || findPath(root->right,path,node))
-            return true;
-        path.pop_back();
-        return false;
-        
-    }
-    
-    // using this node we will find the lowest common ancestor, by comparing the two paths
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> path1, path2;  // we declared the two paths for the two nodes
-       
-        
-        //if either path1 or path2 does not exist, we will return null.
-        if(! findPath(root, path1, p) || ! findPath(root, path2, q))   
+        if(root==NULL)
             return NULL;
-        TreeNode* ans = NULL;
+        if(root==p || root==q)
+            return root;
+        TreeNode* LCA1 = lowestCommonAncestor(root->left,p,q);
+        TreeNode* LCA2 = lowestCommonAncestor(root->right,p,q);
         
-        
-        // here we'll find the ans, by comparing the two paths
-        for(int i=0;i<path1.size() && i< path2.size();i++)
-        {
-            if(path1[i]==path2[i])
-                ans = path1[i];
-        }
-        
-        return ans;
+        if(LCA1 != NULL && LCA2 != NULL)
+            return root;
+        if(LCA1 != NULL)
+            return LCA1;
+        else
+            return LCA2;
     }
 };
