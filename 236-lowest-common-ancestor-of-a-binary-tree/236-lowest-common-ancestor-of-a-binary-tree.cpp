663d7ecc3c1e5afbccfd31d6e31ec8d29d4c59be
sandalagrawal01 <86615492+sandalagrawal@users.noreply.github.com>
2022-10-10 02:52:04 +0530

    Time: 34 ms (37.44%), Space: 17.4 MB (5.35%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,7 +9,9 @@
  */
 class Solution {
 public:
+    // this is the first method to find the lowest common ancestor, using path.
     
+    // using this function we find the paths of the given node from the root node.
     bool findPath(TreeNode* root, vector<TreeNode*>&path, TreeNode* node)
     {
         if(root==NULL)
@@ -24,12 +26,18 @@ public:
         
     }
     
+    // using this node we will find the lowest common ancestor, by comparing the two paths
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> path1, path2;
-        if(! findPath(root, path1, p) || ! findPath(root, path2, q))
+        vector<TreeNode*> path1, path2;  // we declared the two paths for the two nodes
+       
+        
+        //if either path1 or path2 does not exist, we will return null.
+        if(! findPath(root, path1, p) || ! findPath(root, path2, q))   
             return NULL;
         TreeNode* ans = NULL;
         
+        
+        // here we'll find the ans, by comparing the two paths
         for(int i=0;i<path1.size() && i< path2.size();i++)
         {
             if(path1[i]==path2[i])
