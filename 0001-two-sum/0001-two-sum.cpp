45647b3068e7ce56c119698d2d52d02533b85ed1
sandalagrawal01 <86615492+sandalagrawal@users.noreply.github.com>
2022-10-14 21:03:50 +0530

    Time: 118 ms (42.37%), Space: 10.3 MB (74.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,6 +1,38 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+    
+        //another solution is using only 1 for loop, storing the element in an iterator and using the find function we try to find the (target-it) number
+
+vector<int>result;
+for(auto it = nums.begin(); it!= nums.end();++it){
+auto it2 = find(it+1, nums.end(), target-*it);
+if(it2!=nums.end())
+{
+result.push_back(it-nums.begin());
+result.push_back(it2-nums.begin());
+}
+}
+return result;
+        
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        /*first solution, by me, very noob
         int n = nums.size();
         vector<int>ans;
         for(int i=0;i<n;i++){
@@ -14,6 +46,8 @@ public:
             }
         }
         
-        return ans;
+        return ans;*/
+        
     }
 };
+
