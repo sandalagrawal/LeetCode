b7ee797ad543cf87bcf39df7d62eccff089dcf04
sandalagrawal01 <86615492+sandalagrawal@users.noreply.github.com>
2022-10-22 22:48:28 +0530

    Time: 404 ms (30.63%), Space: 51.6 MB (47.41%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -12,6 +12,9 @@ public:
                 if(visited[x]==false){
                     if(dfsrec(x,visited,dfsvisited,graph, present_cycle)){
                        return  present_cycle[i]= true;
+                        // jahan bhi cycle hogi, i.e. true ki condition me hum present cycle me source pr true kr denge!! 
+                        
+                        
                  }
                 } else if(visited[x]==true && dfsvisited[x]==true){
                   return  present_cycle[i] = true;
@@ -36,6 +39,7 @@ public:
         vector<int> ans;
         
         vector<bool> present_cycle(n , false);
+        // here we'll make a vector of bool kind to store which element has a cycle to it. 
         
         for(int i=0;i<n;i++){
             if(visited[i]==false){
@@ -43,6 +47,8 @@ public:
             }
         }
         
+        
+        // iss for loop me hum sare present_cycle==true wale hata denge and we'll get our ans!
         for(int i=0;i<n;i++){
             if(!present_cycle[i])
                 ans.push_back(i);
