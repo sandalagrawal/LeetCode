223a6ad190ea052c85f2133f9e0ea41a3cb316ad
sandalagrawal01 <86615492+sandalagrawal@users.noreply.github.com>
2022-11-04 02:34:03 +0530

    Time: 8 ms (84.01%), Space: 13 MB (94.87%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,83 @@
+class Solution {
+public:
+    
+    bool isValid(int x, int y, int n, int m ,vector<vector<int>>& grid ){
+        if(x>=0 &&x<n && y>=0 && y<m && grid[x][y]==1)
+            return true;
+        return false;
+    }
+    
+    
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n= grid.size();
+        int m = grid[0].size();
+        
+        int time=0;
+        int fresh=0;
+        queue <pair<int,int>> q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2)
+                {
+                    q.push({i,j});
+                }
+                else if(grid[i][j]==1)
+                {
+                    fresh++;
+                }
+            }
+        }
+         if(fresh==0){return 0;}   // agr koi fresh orange hai hi nhi grid me toh chlne ka mtlb hi nhi, ans=0
+        
+        // start BFS traversal
+        while(!q.empty()){
+            
+            int size_q = q.size();
+            int temp=0;
+            while(size_q!=0){
+                
+                pair<int,int> p = q.front();
+                q.pop();
+                
+                int x1 = p.first;
+                int y1 = p.second;
+                
+                int ax[4] = {1,-1,0,0};
+                int ay[4] = {0,0,1,-1};
+                
+                for(int i=0;i<4;i++){
+                    int x = ax[i] + x1;
+                    int y = ay[i] + y1;
+                    
+                    if(isValid(x,y,n,m,grid)){
+                        temp++;
+                        grid[x][y]=2;
+                        q.push({x,y});
+                    }
+                }
+                
+                size_q--;
+            }
+            if(temp!=0)time++;
+
+        }
+        
+        
+        
+        
+        
+        // to check if any fresh oranges are left in the grid, to return -1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                     time=0;
+                    break;
+                }
+                   
+                
+            }
+        }
+       
+        return (time==0) ?-1 : time;
+    }
+};
